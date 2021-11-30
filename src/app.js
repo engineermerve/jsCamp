80f94e4d69e76d3a626c387aa41962d1009e75bf
engineermerve <demirmerve1996@gmail.com>
2021-11-30 10:08:46 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ console.log("Merhaba Kodlama.io")
 //JS type safe değildir
 let dolarBugun = 9.30
 
-let dolarDun = 9.20
-dolarDun = "9.20"
+let dolarDun = 9.21
+dolarDun = "9.21"
 {
     let dolarDun = 9.10
 }
